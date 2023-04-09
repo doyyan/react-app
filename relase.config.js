d8132c478719d57cc6e8556d0cc8e0c321a1b743
Ravi <rd_doyyan@yahoo.com>
2023-04-09 09:18:55 +0100

    feat: new-feature, release and versioning

--- relase.config.js
+++ relase.config.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/git",
+    "@semantic-release/github",
   ],
   branches: "master",
 };
