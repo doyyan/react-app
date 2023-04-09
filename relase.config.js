baaff7b36a91e474c1efd03baa509090ca53bc8e
Ravi <rd_doyyan@yahoo.com>
2023-04-09 11:04:25 +0100

    fix: update the Zip file names

--- relase.config.js
+++ relase.config.js
@@ -3,7 +3,18 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "build.zip",
+            label: "Build",
+          },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
   ],
   branches: "master",
 };
