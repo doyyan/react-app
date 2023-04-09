188e276d4312c52ee572a910e6a99c18607a604b
Ravi <rd_doyyan@yahoo.com>
2023-04-09 08:24:19 +0100

    feat: new-feature, release and versioning

--- relase.config.js
+++ relase.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  branches: "master",
   repositoryUrl: "https://github.com/doyyan/react-app",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/git",
   ],
+  branches: ["master"],
 };
