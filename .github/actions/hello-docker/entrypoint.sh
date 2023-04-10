115e2a7940466bda9a0ff668343b960d03123726
Ravi <rd_doyyan@yahoo.com>
2023-04-10 15:19:38 +0100

    new docker action

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -l
+
+echo "::debug ::Debug Message"
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+
+echo "::add-mask::$1"
+
+time=$(date)
+
+echo "::set-output name=time::$time"
+
+echo "::sgroup:: Some expandable logs"
+echo 'some stuff'
+echo 'some stuff'
+echo 'some stuff'
+echo 'some stuff'
+echo 'some stuff'
+echo "::endgroup::"
+
+
+echo'::set-env name=HELLO::hello"
+
+
+echo "Hello $1"
