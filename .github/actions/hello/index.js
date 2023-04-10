24b55ff7b819102c15cc54f1981f4328a92eac79
Ravi <rd_doyyan@yahoo.com>
2023-04-10 08:56:33 +0100

    new action

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,17 +1,15 @@
-try {
-    // throw( new Error())
-
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const name = core.getInput('who-to-greet')
-core.setOutput("time", time.toTimeString())
+try {
+  // throw( new Error()
 
-console.log(`Hello ${name}`);
+  const name = core.getInput("who-to-greet");
+  core.setOutput("time", time.toTimeString());
 
-console.log(JSON.stringify(github, null, '\t'))
+  console.log(`Hello ${name}`);
+
+  console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
-    core.setFailed(error.message)
-    
+  core.setFailed(error.message);
 }
-
